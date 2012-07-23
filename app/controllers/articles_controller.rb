a6d86c7206d9645a6e9fca4781954099f34f03bc
Asad Akbar + Sherief Gharraph <apprentice+aa+sg@devbootcamp.com>
2012-07-23 12:19:36 -0700

    debugging exercise complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(params[:article])
 
     @article.save
-
+    audit @article.inspect
 
     redirect_to article_path(@article)
   end
